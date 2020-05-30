ea41c09f3423d1b6d69a56dc711900e757e5adcf
Ben Lehmann <62218426+BenLehmann12@users.noreply.github.com>
2020-05-30 14:20:18 -0500

    Update Audio.py

--- Audio.py
+++ Audio.py
@@ -10,45 +10,45 @@ import numpy as np
 
 path = 'island_music_x.wav'
 x, sr = librosa.load(path)
-#print(x.shape,sr)     #x.shape = (276480,) sr = 22050
+print(x.shape,sr)     #x.shape = (276480,) sr = 22050
 
 plt.figure(figsize=(14,5))
 librosa.display.waveplot(x, sr=sr)
-#plt.show()
+plt.show()
 
-#X = librosa.stft(x)
-#Xdb = librosa.amplitude_to_db(abs(X))
-#plt.figure(figsize=(14,5))
-#librosa.display.specshow(Xdb,sr=sr, x_axis='time',y_axis='log')
-#plt.colorbar()
-#plt.show()
+X = librosa.stft(x)
+Xdb = librosa.amplitude_to_db(abs(X))
+plt.figure(figsize=(14,5))
+librosa.display.specshow(Xdb,sr=sr, x_axis='time',y_axis='log')
+plt.colorbar()
+plt.show()
 
 #Perceptual Weighting:
-#freq = librosa.core.fft_frequencies(sr=sr)
-#mag = librosa.perceptual_weighting(abs(X)**2, freq)
-#librosa.display.specshow(mag, sr=sr, x_axis='time', y_axis='log')
-#plt.colorbar()
-#plt.show()
+freq = librosa.core.fft_frequencies(sr=sr)
+mag = librosa.perceptual_weighting(abs(X)**2, freq)
+librosa.display.specshow(mag, sr=sr, x_axis='time', y_axis='log')
+plt.colorbar()
+plt.show()
 
 r = librosa.autocorrelate(x, max_size=6000)
-#sample = r[:300]
-#plt.figure(figsize=(14,5))
-#plt.plot(sample)
-#plt.show()
+sample = r[:300]
+plt.figure(figsize=(14,5))
+plt.plot(sample)
+plt.show()
 
 #Chroma Features
 sound_len = 400
-#chrom = librosa.feature.chroma_stft(x, sr=sr, hop_length=sound_len)
-#plt.figure(figsize=(14,5))
-#librosa.display.specshow(chrom, x_axis='time',y_axis='chroma', hop_length=sound_len)
-#plt.colorbar()
-#plt.show()
+chrom = librosa.feature.chroma_stft(x, sr=sr, hop_length=sound_len)
+plt.figure(figsize=(14,5))
+librosa.display.specshow(chrom, x_axis='time',y_axis='chroma', hop_length=sound_len)
+plt.colorbar()
+plt.show()
 
 #Mel-power
-#S = librosa.feature.melspectrogram(x,sr=sr,n_fft=120)
-#log = librosa.power_to_db(S, ref=np.max)
-#plt.figure(figsize=(14,5))
-#librosa.display.specshow(log,sr=sr,x_axis='time',y_axis='mel')
-#plt.colorbar()
-#plt.show()
+S = librosa.feature.melspectrogram(x,sr=sr,n_fft=120)
+log = librosa.power_to_db(S, ref=np.max)
+plt.figure(figsize=(14,5))
+librosa.display.specshow(log,sr=sr,x_axis='time',y_axis='mel')
+plt.colorbar()
+plt.show()
 
